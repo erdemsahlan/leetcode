54f33927787cef9f2962448f0c697f3d54838fa4
Erdem Şahlan <sahlanerdem@hotmail.com>
2024-12-19 21:29:23 +0300

    up

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -9,6 +9,8 @@ using System.Xml.Linq;
 using Microsoft.VisualBasic;
 using System.Threading.Channels;
 using System.Xml;
+using System.Diagnostics;
+using System.Transactions;
 
 //namespace mergeSortedArray // Note: actual namespace depends on the project name.
 //{
@@ -302,7 +304,7 @@ using System.Xml;
 
 //The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
- 
+
 
 //Example 1:
 
@@ -353,4 +355,73 @@ using System.Xml;
 //    }
 //}
 
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+//Example 1:
+
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+//Example 2:
+
+//Input: prices = [7,6,4,3,1]
+//Output: 0
+//Explanation: In this case, no transactions are done and the max profit = 0.You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+//Example 1:
+
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+//Example 2:
+
+//Input: prices = [7,6,4,3,1]
+//Output: 0
+//Explanation: In this case, no transactions are done and the max profit = 0.
+//121.Best Time to Buy and Sell Stock
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] nums = [1, 2, 3, 4, 5];
+//        var a = solutions(nums);
+//        Console.WriteLine(a.ToString());
+//    }
+
+//    static int solutions(int[] nums)
+//    {
+//        int maxPrice = 0;
+//        int price = 0;
+//        for (int i = 0; i < nums.Length-1; i++)
+//        {
+//            for (int j = i; j < nums.Length-1; j++)
+//            {
+//                price = nums[j+1] - nums[i];
+//                if (price > maxPrice)
+//                {
+//                    maxPrice = price;
+//                }
+//                else
+//                {
+//                    price = 0;
+//                }
+//            }
+//        }
+//        return maxPrice;
+//    }
+//}
 
